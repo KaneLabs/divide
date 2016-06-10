f9ac07d078231523ca2df35bdc618dae0dce31c7
Ryan Kane <Kane@Ryans-MacBook-Pro.local>
2016-06-10 13:03:45 -0600

    initial commit

---
+++ divide/migrations/20160610130243_createBoardPosts.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('name').unique().notNullable();
+    table.string('password').notNullable();
+    table.string('email').unique().notNullable();
+    table.integer('profileimgid').defaultTo(1);
+    table.integer('tribeid');
+    table.integer('respect').defaultTo(0);
+    table.integer('disrespect').defaultTo(0);
+    table.string('oauthtoken').unique();
+    table.boolean('isgod').defaultTo(false);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return schema.dropTable('users');
+};
