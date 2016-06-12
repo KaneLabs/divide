c75010b98da02883f04d5c232e338708fc859585
Ryan Kane <Kane@Ryans-MacBook-Pro.local>
2016-06-12 13:27:56 -0600

    i dont even know

--- knexfile.js
+++ knexfile.js
@@ -7,28 +7,12 @@ module.exports = {
     connection: 'postgres://localhost:5432/dividedb'
   },
 
-  staging: {
-    client: 'postgres',
+  production: {
+    client: 'postgresql',
     connection: {
       database: 'dividedb.c4sisckqzvvb.us-east-1.rds.amazonaws.com:5432',
       user:     'dividedb',
       password: 'vdXq*#*YVNB5'
-    },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
     },
     pool: {
       min: 2,
